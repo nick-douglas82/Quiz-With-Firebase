7bfe6f80aa52cab686a54ec6e9a95fa6fce19dd9
Nick Douglas <nick.douglas82@gmail.com>
2020-08-24 16:47:19 +0100

    Changed to call from api rather than dummy questions

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react'
 import Question from './Question';
 
-const dummQuestion = {
-  question: "What's the best programming language?",
-  answerChoices: ['JavaScript', 'Java', 'C#', 'Swift']
-}
 export default class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: null,
+      currentQuestion: null
+    }
+  }
+
+  async componentDidMount() {
+    const url = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple';
+
+    try {
+      const res = await fetch(url);
+      const { results } = await res.json();
+
+      const questions = results.map(loadedQuestion => {
+        const formattedQuestion = {
+          question: loadedQuestion.question,
+          answerChoices: [...loadedQuestion.incorrect_answers]
+        };
+
+        formattedQuestion.answer = Math.floor(Math.random() * 4);
+        formattedQuestion.answerChoices.splice(formattedQuestion.answer, 0, loadedQuestion.correct_answer)
+
+        return formattedQuestion;
+      })
+      this.setState({questions, currentQuestion: questions[0]})
+    } catch (err) {
+      console.error(err);
+    }
+  }
   render() {
     return (
       <>
-        <Question question={dummQuestion} />
+        {this.state.currentQuestion && (
+          <Question question={this.state.currentQuestion} />
+        )}
       </>
     )
   }

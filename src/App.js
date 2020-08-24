1ccfa9401de76086da2acd137b0d4edaf9948a14
Nick Douglas <nick.douglas82@gmail.com>
2020-08-24 16:16:36 +0100

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import Home from './components/Home';
 import { Route, BrowserRouter as Router} from 'react-router-dom';
+import Home from './components/Home';
+import Game from './components/Game';
+import HighScores from './components/HighScores';
+
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Route path="/" component={ Home } />
-      <Route path="/game" component={ Game } />
-      <Route path="/highScores" component={ HighScores } />
+      <div className="container">
+        <Route exact path="/" component={ Home } />
+        <Route path="/game" component={ Game } />
+        <Route path="/highScores" component={ HighScores } />
+      </div>
     </Router>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import Question from './Question';
 
+const dummQuestion = {
+  question: "What's the best programming language?",
+  answerChoices: ['JavaScript', 'Java', 'C#', 'Swift']
+}
 export default class Game extends Component {
   render() {
     return (
-      <div>
-        GAME
-      </div>
+      <>
+        <Question question={dummQuestion} />
+      </>
     )
   }
 }
